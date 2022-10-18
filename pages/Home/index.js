73256744b793a385443f6db7d49876cce2ae957b
melendez-dev <sams20030818@gmail.com>
2022-10-17 20:00:04 -0500

    feature: delete home folder on pages and integreted slider home

--- pages/Home/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../../src/components/shared/Layout/Layout"
-import Home from "../../src/components/common/Home/Home"
-export default function PageHome (){
-    return(
-        <Layout>
-            <Home />
-        </Layout>
-    )
-}
