77db158ae68b64a272436193138ecf97843a35e6
melendez-dev <sams20030818@gmail.com>
2022-10-17 21:47:55 -0500

    feature: finish home page

--- src/components/common/Home/Home.js
+++ src/components/common/Home/Home.js
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import SimpleImageSlider from "react-simple-image-slider";
 import { Slide } from "react-slideshow-image";
-import { TextField, Box, Typography, Button, Container } from "@mui/material";
+import {
+  TextField,
+  Box,
+  Typography,
+  Button,
+  Container,
+  Grid,
+} from "@mui/material";
 export default function Home() {
-  const images = [
-    "/images/SlidersHome/slide_one.png",
-    "/images/SlidersHome/slide_two.png",
-    "/images/SlidersHome/slide_three.png",
+  const slideImages = [
+    {
+      url: "/images/SlidersHome/slide_one.png",
+    },
+    {
+      url: "/images/SlidersHome/slide_two.png",
+    },
+    {
+      url: "/images/SlidersHome/slide_three.png",
+    },
   ];
-  // create a interator of images
   return (
     <Box
       style={{
@@ -19,16 +31,56 @@ export default function Home() {
           position: "absolute",
           top: "50%",
           zIndex: 10,
-          left: "50%",
+          left: "30%",
+          transform: "translate(-50%, -50%)",
         }}
       >
-        <Typography variant="h1-bold" color="#ffff">
-          adfgfdws
-        </Typography>
+        <Grid container>
+          <Grid item xs={12}>
+            <Box>
+              <Typography variant="h1-bold" color="#fff">
+                ¡CONSTRUIR CASAS, <br /> ES SOLO EL <br /> COMIENZO!
+              </Typography>
+            </Box>
+            <Box mt={2}>
+              <Typography variant="p" color="#fff">
+                Trabajamos por una sociedad justa, sin pobreza. <br /> Damos
+                esperanza a la población más vulnerable en el <br /> barrio “Las
+                Flores” en la ciudad de Barranquilla, Colombia
+              </Typography>
+            </Box>
+            <Box mt={3}>
+              <Button variant="outlined">
+                <span
+                  style={{
+                    color: "#fff",
+                  }}
+                >
+                  Involucráte
+                </span>
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
-			<Slide duration={4000}>
 
-			</Slide>
+      <Slide duration={4000}>
+        {slideImages.map((slideImage, index) => (
+          <div className="each-slide" key={index}>
+            <div
+              style={{
+                backgroundImage: `url(${slideImage.url})`,
+                height: "705px",
+                backgroundSize: "cover",
+                backgrounRepeat: "no-repeat",
+                backgroundPosition: "center",
+                // change black opacity
+                //opacity: "0.8",
+              }}
+            ></div>
+          </div>
+        ))}
+      </Slide>
     </Box>
   );
 }
