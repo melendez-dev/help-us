e31d2c09deb1caed919e821323013508ca020771
melendez-dev <sams20030818@gmail.com>
2022-10-24 16:46:36 -0500

    feature: responsive home page

--- src/components/common/Home/Home.js
+++ src/components/common/Home/Home.js
@@ -9,8 +9,17 @@ import {
   Container,
   Grid,
 } from "@mui/material";
+
+// redux
+import { useSelector } from "react-redux";
 export default function Home() {
+  // redux
+  const movil = useSelector((state) => state.movil.value);
+
+  // router
   const router = useRouter();
+
+  // images
   const slideImages = [
     {
       url: "/images/SlidersHome/slide_one.png",
@@ -22,51 +31,65 @@ export default function Home() {
       url: "/images/SlidersHome/slide_three.png",
     },
   ];
+
+  const CustomContainer = movil ? Container : Box;
+
   return (
     <Box
       style={{
         position: "relative",
+        width: "100%",
       }}
     >
       <Box
         style={{
           position: "absolute",
-          top: "50%",
+          top: movil ? "30%" : "50%",
           zIndex: 10,
-          left: "30%",
-          transform: "translate(-50%, -50%)",
+          left: movil ? undefined : "30%",
+          transform: movil ? undefined : "translate(-50%, -50%)",
         }}
       >
-        <Grid container>
-          <Grid item xs={12}>
-            <Box>
-              <Typography variant="h1-bold" color="#fff">
-                ¡CONSTRUIR CASAS, <br /> ES SOLO EL <br /> COMIENZO!
-              </Typography>
-            </Box>
-            <Box mt={2}>
-              <Typography variant="p" color="#fff">
-                Trabajamos por una sociedad justa, sin pobreza. <br /> Damos
-                esperanza a la población más vulnerable en el <br /> barrio “Las
-                Flores” en la ciudad de Barranquilla, Colombia
-              </Typography>
-            </Box>
-            <Box mt={3}>
-              <Button variant="outlined" onClick={() => router.push("/donate")}>
-                <span
-                  style={{
-                    color: "#fff",
-                  }}
+        <CustomContainer>
+          <Grid container>
+            <Grid item xs={12}>
+              <Box>
+                <Typography
+                  variant={movil ? "h2-bold" : "h1-bold"}
+                  color="#fff"
                 >
-                  Involucráte
-                </span>
-              </Button>
-            </Box>
+                  ¡CONSTRUIR CASAS, <br /> ES SOLO EL {!movil && <br />}{" "}
+                  COMIENZO!
+                </Typography>
+              </Box>
+              <Box mt={2}>
+                <Typography variant={movil ? "p-small" : "p"} color="#fff">
+                  Trabajamos por una sociedad justa, sin pobreza. <br /> Damos
+                  esperanza a la población más vulnerable en el <br /> barrio
+                  “Las Flores” en la ciudad de Barranquilla, Colombia
+                </Typography>
+              </Box>
+              <Box mt={3}>
+                <Button
+                  variant="outlined"
+                  onClick={() => router.push("/donate")}
+                >
+                  <span
+                    style={{
+                      color: "#fff",
+                      fontSize: movil ? "10px" : undefined,
+                    }}
+                  >
+                    Involucráte
+                  </span>
+                </Button>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
+        </CustomContainer>
       </Box>
 
-      <Slide duration={4000} autoplay>
+      <Slide duration={4000} autoplay arrows={movil ? false : true}>
         {slideImages.map((slideImage, index) => (
           <div className="each-slide" key={index}>
             <div
